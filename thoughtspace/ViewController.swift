a5e0677e1eaac668a45252c23abdaaf0a0e5f90c
king <mto0006@auburn.edu>
2025-09-17 05:58:44 -0500

    Layout foundation

---
+++ thoughtspace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  thoughtspace
+//
+//  Created by Midas Oden on 9/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ thoughtspaceTests/thoughtspaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  thoughtspaceTests.swift
+//  thoughtspaceTests
+//
+//  Created by Midas Oden on 9/15/25.
+//
+
+import Testing
+@testable import thoughtspace
+
+struct thoughtspaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
